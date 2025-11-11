5af18144765d7e0bb75da2d5fe3183986498fcf5
aryan1403 <aaryan14032006@email.com>
2025-11-11 17:00:50 +0530

    done

--- provider.test.js
+++ provider.test.js
@@ -7,20 +7,20 @@ dotenv.config();
 
 describe("Pact Verification", () => {
   test("validates the expectations of TodoConsumer", async () => {
-    // const opts = {
-    //   providerBaseUrl: "http://localhost:3000",
-    //   pactUrls: [path.resolve(process.cwd(), "pacts/TodoConsumer-TodoProvider.json")],
-    // };
-
     const opts = {
       providerBaseUrl: "http://localhost:3000",
-      pactBrokerUrl: process.env.PACT_BROKER_BASE_URL,
-      provider: "TodoProvider",
-      consumerVersionSelectors: [{ latest: true }],
-      publishVerificationResult: true,
-      providerVersion: "1.0.0",
-      pactBrokerToken: process.env.PACT_BROKER_TOKEN,
+      pactUrls: [path.resolve(process.cwd(), "pacts/TodoConsumer-TodoProvider.json")],
     };
+
+    // const opts = {
+    //   providerBaseUrl: "http://localhost:3000",
+    //   pactBrokerUrl: process.env.PACT_BROKER_BASE_URL,
+    //   provider: "TodoProvider",
+    //   consumerVersionSelectors: [{ latest: true }],
+    //   publishVerificationResult: true,
+    //   providerVersion: "1.0.0",
+    //   pactBrokerToken: process.env.PACT_BROKER_TOKEN,
+    // };
 
     const verifier = new Verifier(opts);
     const output = await verifier.verifyProvider();
